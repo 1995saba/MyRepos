8183b2a1635ca672d8d93e0eee2ba4694ca414b9
Sabina <sabina.orunbaeva@gmail.com>
2017-07-24 23:29:21 +0600

    Add files via upload

---
+++ Homework2/Task8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение объема первого тела (м куб.):");
+            string volumeOneAsString = Console.ReadLine();
+            int volumeOne = Int32.Parse(volumeOneAsString);
+
+            Console.WriteLine("Введите значение массы первого тела (кг):");
+            string massOneAsString = Console.ReadLine();
+            int massOne = Int32.Parse(massOneAsString);
+
+            Console.WriteLine("Введите значение объема второго тела (м куб.):");
+            string volumeTwoAsString = Console.ReadLine();
+            int volumeTwo = Int32.Parse(volumeTwoAsString);
+
+            Console.WriteLine("Введите значение массы второго тела (кг):");
+            string massTwoAsString = Console.ReadLine();
+            int massTwo = Int32.Parse(massTwoAsString);
+
+            double densityOne = massOne / volumeOne;
+            double densityTwo = massTwo / volumeTwo;
+
+            if (densityOne > densityTwo)
+            {
+                Console.WriteLine("Первое тело имеет большую плотность.");
+            }
+
+            else if (densityOne > densityTwo)
+            {
+                Console.WriteLine("Второе тело имеет большую плотность.");
+            }
+
+            else if (densityOne == densityTwo)
+            {
+                Console.WriteLine("Тела имеют равную плотность.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework2/Task9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение сопротивления первого участка электрической цепи (Ом):");
+            string rOneAsString = Console.ReadLine();
+            int rOne = Int32.Parse(rOneAsString);
+
+            Console.WriteLine("Введите значение напряжения первого участка электрической цепи (Вольт):");
+            string uOneAsString = Console.ReadLine();
+            int uOne = Int32.Parse(uOneAsString);
+
+            Console.WriteLine("Введите значение сопротивления второго участка электрической цепи (Ом):");
+            string rTwoAsString = Console.ReadLine();
+            int rTwo = Int32.Parse(rTwoAsString);
+
+            Console.WriteLine("Введите значение напряжения второго участка электрической цепи (Вольт):");
+            string uTwoAsString = Console.ReadLine();
+            int uTwo = Int32.Parse(uTwoAsString);
+
+            double iOne = uOne / rOne;
+            double iTwo = uTwo / rTwo;
+
+            if (iOne > iTwo)
+            {
+                Console.WriteLine("Меньший ток протекает по второму участку.");
+            }
+
+            else if (iOne < iTwo)
+            {
+                Console.WriteLine("Меньший ток протекает по первому участку.");
+            }
+
+            else if (iOne == iTwo)
+            {
+                Console.WriteLine("Оба участка имеют равную силу тока.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
