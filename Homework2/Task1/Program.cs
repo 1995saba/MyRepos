1f78e6f6f6d4223c4d3ccdf2fc724f9560933fa9
Sabina <sabina.orunbaeva@gmail.com>
2017-07-24 23:28:10 +0600

    Add files via upload

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(100) + "  " + rand.Next(100) + "  " + rand.Next(100));
+            Console.ReadLine();
+        }
+    }
+}
