806bd3aea99a86b998331c1516a90833df0997f2
Sabina <sabina.orunbaeva@gmail.com>
2017-08-16 17:58:49 +0600

    Add files via upload

---
+++ RangeOfArray/ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray rangeOfArrays = new RangeOfArray();
+            
+            Console.WriteLine("Введите нижний индекс допустимого диапазона: ");
+            string subscriptAsString = Console.ReadLine();
+            rangeOfArrays.Subscript = Int32.Parse(subscriptAsString);
+
+            Console.WriteLine("Введите верхний индекс допустимого диапазона: ");
+            string superscriptAsString = Console.ReadLine();
+            rangeOfArrays.Superscript = Int32.Parse(superscriptAsString);
+
+            for(int i = rangeOfArrays.Subscript; i <= rangeOfArrays.Superscript; i++)
+            {
+                Console.WriteLine("a[" + i + ']');
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RangeOfArray/ConsoleApp4/RangeOfArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class RangeOfArray
+    {
+        private int subscript;
+        private int superscript;
+
+        public RangeOfArray()
+        {
+            subscript = 0;
+            superscript = 10;
+        }
+        public RangeOfArray( int subscript, int superscript)
+        {
+            this.subscript = subscript;
+            this.superscript = superscript;
+        }
+
+        public int Subscript { get; set; }
+
+        public int Superscript { get; set; }
+    }
+}
