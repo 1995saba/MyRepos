d263cba4e2203b10bbd28e38e7321622c9362306
Sabina <sabina.orunbaeva@gmail.com>
2017-08-25 17:39:35 +0600

    Add files via upload

---
+++ PracticeCollection/ConsoleApp7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayListSize;
+            Console.WriteLine("Введите значение для количества элементов в ArrayList: ");
+            string arrayListSizeAsString = Console.ReadLine();
+            bool result = Int32.TryParse(arrayListSizeAsString, out arrayListSize);
+            if (!result)
+            {
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
+                                   arrayListSizeAsString == null ? "<null>" : arrayListSizeAsString);
+            }
+            Console.WriteLine();
+
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < arrayListSize; i++)
+            {
+                arrayList.Add(i);
+                Console.WriteLine(arrayList[i]);
+            }
+            Console.WriteLine();
+
+            ArrayList newArrayList = new ArrayList();
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    newArrayList.Add(arrayList[i]);
+                }
+            }
+            arrayList.Clear();
+
+            for (int i = 0; i < newArrayList.Count; i++)
+            {
+                Console.WriteLine(newArrayList[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticeCollection/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int listSize;
+            Console.WriteLine("Введите значение для количества элементов в коллекции: ");
+            string listSizeAsString = Console.ReadLine();
+            bool result = Int32.TryParse(listSizeAsString, out listSize);
+            if (!result)
+            {
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
+                                   listSizeAsString == null ? "<null>" : listSizeAsString);
+            }
+            Console.WriteLine();
+
+            int sum=0;
+            var numbers=new List<int>();
+            for (int i = 0; i < listSize; i++)
+            {
+                numbers.Add(i);
+                Console.WriteLine(numbers[i]);
+                sum += i;
+            }
+            double average = sum / listSize;
+            Console.WriteLine();
+
+            for (int i = 0; i < listSize; i++)
+            {
+                if (numbers[i]>average) {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
