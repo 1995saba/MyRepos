7680c00b84d507c7dc9cabb6a3b38ddde06079c5
Sabina <sabina.orunbaeva@gmail.com>
2017-09-11 18:25:15 +0600

    Add files via upload

---
+++ HomeworkSerializer/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookCollection = new List<Book>();
+            int size = 10;
+            Book book = new Book();
+            string path = @"C:\data";
+
+            for(int i=0; i < size; i++)
+            {
+                book.Name = "asd";
+                book.Price = 100*i;
+                book.Author = "asd";
+                book.Year = 1990 + i;
+
+                bookCollection.Add(book);
+            }
+
+            FileStream stream = new FileStream(path + @"\book.dat", FileMode.Create);
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(stream, bookCollection);
+            }
+            catch(SerializationException ex)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ HomeworkSerializer/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookCollection = new List<Book>();
+            string path = @"C:\data";
+            FileStream stream = new FileStream(path + @"\book.dat", FileMode.Open);
+
+        }
+    }
+}
